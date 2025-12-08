2fa5f4aadfcacd18dad36a156363b2080665a164
sanjai042004 <sanjaiv642@gmail.com>
2025-12-08 09:24:55 +0530

    .

--- portfolio/src/components/BackToTop.jsx
+++ portfolio/src/components/BackToTop.jsx
@@ -14,8 +14,17 @@ export const BackToTop = () => {
     visible && (
       <button
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        className="fixed bottom-4 right-4 md:bottom-6 md:right-6backdrop-blur-xl bg-white/10border border-white/20 shadow-xl
-          p-2 md:p-3rounded-full text-white transition-all duration-300 hover:bg-white/20 hover:scale-110 hover:shadow-2xl cursor-pointer"
+        className="
+          fixed bottom-4 right-4
+          md:bottom-6 md:right-6
+          backdrop-blur-xl bg-white/10
+          border border-white/20 shadow-xl
+          p-2 md:p-3
+          rounded-full text-white
+          transition-all duration-300
+          hover:bg-white/20 hover:scale-110 hover:shadow-2xl
+          cursor-pointer
+        "
       >
         <ArrowUp size={18} className="md:size-6" />
       </button>
